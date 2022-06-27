4c57f4a63321d53c4b1537fcabdc508d1fe03849
s24544 <61560856+s24544@users.noreply.github.com>
2022-06-27 11:17:16 +0200

    Delete src directory

--- src/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$tmp = isset($_POST['logged']);
-session_destroy();
-session_start();
-$_SESSION['btnLogout'] = $tmp;
-header('Location: index.php');
--- src/mail.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Mail wysłany";
-?>
